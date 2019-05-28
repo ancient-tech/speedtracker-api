2d97fa73b884950ae8f44f4ea70c34e478cf313a
praWeb <prasanna.gaddam@shiftcommerce.com>
2019-05-28 11:04:03 +0100

    intial repository

---
+++ config.js
@@ -0,0 +1,111 @@
+var convict = require('convict')
+
+var conf = convict({
+  env: {
+    doc: 'The applicaton environment',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV'
+  },
+  port: {
+    doc: 'The port to bind',
+    format: 'port',
+    default: 0,
+    env: 'PORT'
+  },
+  wpt: {
+    key: {
+      doc: 'WPT API key',
+      format: String,
+      default: null,
+      env: 'WPT_KEY'
+    },
+    url: {
+      doc: 'WPT API URL',
+      format: String,
+      default: 'https://www.webpagetest.org',
+      env: 'WPT_URL'
+    }
+  },
+  githubToken: {
+    doc: 'GitHub access token',
+    format: String,
+    default: null,
+    env: 'GITHUB_TOKEN'
+  },
+  email: {
+    sparkboxApiKey: {
+      doc: 'Sparkbox API key',
+      format: String,
+      default: null,
+      env: 'SPARKBOX_API_KEY'
+    }
+  },
+  database: {
+    uri: {
+      doc: 'Mongo database connection URI',
+      format: String,
+      default: null,
+      env: 'MONGODB_URI'
+    },
+    reposCollection: {
+      doc: 'Name of the collection to be used for storing repositories',
+      format: String,
+      default: 'st_repos',
+      env: 'COLLECTION_REPOS'
+    }
+  },
+  scheduling: {
+    checkInterval: {
+      doc: 'Interval at which the Scheduler checks for tests to run (in milliseconds)',
+      format: Number,
+      default: 900000,
+      env: 'SCHEDULING_CHECK_INTERVAL'
+    },
+    minimumInterval: {
+      doc: 'Minimum interval allowed for scheduled tests (in hours)',
+      format: Number,
+      default: 12,
+      env: 'SCHEDULING_MIN_INTERVAL'
+    }
+  },
+  analytics: {
+    googleAnalyticsId: {
+      doc: 'Google Analytics account ID',
+      format: String,
+      default: null,
+      env: 'GOOGLE_ANALYTICS_ID'
+    }
+  },
+  raygunApiKey: {
+    doc: 'Raygun API key',
+    format: String,
+    default: '',
+    env: 'RAYGUN_APIKEY'
+  },
+  blockList: {
+    doc: 'Comma-separated list of GitHub usernames to block',
+    format: String,
+    default: '',
+    env: 'BLOCK_LIST'
+  },
+  pagespeedApiKey: {
+    doc: 'Google PageSpeed Insights API key',
+    format: String,
+    default: '',
+    env: 'PAGESPEED_API_KEY'
+  }
+})
+
+try {
+  var env = conf.get('env')
+
+  conf.loadFile(__dirname + '/config.' + env + '.json')
+  conf.validate()
+
+  console.log('(*) Local config file loaded')
+} catch (e) {
+  
+}
+
+module.exports = conf
---
+++ index.js
@@ -0,0 +1,179 @@
+'use strict'
+
+const Analytics = require('./lib/Analytics')
+const config = require('./config')
+const cors = require('cors')
+const crypto = require('crypto')
+const Database = require('./lib/Database')
+const ErrorHandler = require('./lib/ErrorHandler')
+const express = require('express')
+const GitHub = require('./lib/GitHub')
+const Scheduler = require('./lib/Scheduler')
+const SpeedTracker = require('./lib/SpeedTracker')
+
+// ------------------------------------
+// Server
+// ------------------------------------
+
+const server = express()
+
+server.use(cors())
+
+// ------------------------------------
+// Scheduler
+// ------------------------------------
+
+let scheduler
+
+// ------------------------------------
+// GitHub
+// ------------------------------------
+
+const github = new GitHub()
+
+github.authenticate(config.get('githubToken'))
+
+// ------------------------------------
+// DB connection
+// ------------------------------------
+
+let db = new Database(connection => {
+  console.log('(*) Established database connection')
+
+  server.listen(config.get('port'), () => {
+    console.log(`(*) Server listening on port ${config.get('port')}`)
+  })
+
+  scheduler = new Scheduler({
+    db: connection,
+    remote: github
+  })
+})
+
+// ------------------------------------
+// Endpoint: Test
+// ------------------------------------
+
+const testHandler = (req, res) => {
+  const blockList = config.get('blockList').split(',')
+
+  // Abort if user is blocked
+  if (blockList.indexOf(req.params.user) !== -1) {
+    ErrorHandler.log(`Request blocked for user ${req.params.user}`)
+
+    return res.status(429).send()
+  }
+
+  const speedtracker = new SpeedTracker({
+    db,
+    branch: req.params.branch,
+    key: req.query.key,
+    remote: github,
+    repo: req.params.repo,
+    scheduler,
+    user: req.params.user
+  })
+
+  let profileName = req.params.profile
+
+  speedtracker.runTest(profileName).then(response => {
+    res.send(JSON.stringify(response))
+  }).catch(err => {
+    ErrorHandler.log(err)
+
+    res.status(500).send(JSON.stringify(err))
+  })
+}
+
+server.get('/v1/test/:user/:repo/:branch/:profile', testHandler)
+server.post('/v1/test/:user/:repo/:branch/:profile', testHandler)
+
+// ------------------------------------
+// Endpoint: Connect
+// ------------------------------------
+
+server.get('/v1/connect/:user/:repo', (req, res) => {
+  const github = new GitHub(GitHub.GITHUB_CONNECT)
+
+  github.authenticate(config.get('githubToken'))
+
+  github.api.users.getRepoInvites({}).then(response => {
+    let invitationId
+    let invitation = response.some(invitation => {
+      if (invitation.repository.full_name === (req.params.user + '/' + req.params.repo)) {
+        invitationId = invitation.id
+
+        return true
+      }
+    })
+
+    if (invitation) {
+      return github.api.users.acceptRepoInvite({
+        id: invitationId
+      })        
+    } else {
+      return Promise.reject()
+    }
+  }).then(response => {
+    // Track event
+    new Analytics().track(Analytics.Events.CONNECT)
+
+    res.send('OK!')
+  }).catch(err => {
+    ErrorHandler.log(err)
+
+    res.status(500).send('Invitation not found.')
+  })  
+})
+
+// ------------------------------------
+// Endpoint: Encrypt
+// ------------------------------------
+
+server.get('/encrypt/:key/:text?', (req, res) => {
+  const key = req.params.key
+  const text = req.params.text || req.params.key
+
+  const cipher = crypto.createCipher('aes-256-ctr', key)
+  let encrypted = cipher.update(decodeURIComponent(text), 'utf8', 'hex')
+
+  encrypted += cipher.final('hex')
+
+  res.send(encrypted)
+})
+
+// ------------------------------------
+// Endpoint: Decrypt
+// ------------------------------------
+
+server.get('/decrypt/:key/:text?', (req, res) => {
+  const decipher = crypto.createDecipher('aes-256-ctr', req.params.key)
+  let decrypted = decipher.update(req.params.text, 'hex', 'utf8')
+
+  decrypted += decipher.final('utf8')
+
+  res.send(decrypted)
+})
+
+// ------------------------------------
+// Endpoint: Catch all
+// ------------------------------------
+
+server.all('*', (req, res) => {
+  const response = {
+    success: false,
+    error: 'INVALID_URL_OR_METHOD'
+  }
+
+  res.status(404).send(JSON.stringify(response))
+})
+
+// ------------------------------------
+// Basic error logging
+// ------------------------------------
+
+process.on('unhandledRejection', (reason, promise) => {
+  if (reason) {
+    ErrorHandler.log(reason)  
+  }
+})
